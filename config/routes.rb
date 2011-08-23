d9a864c80e4d746798e176cddbd4aab9ed6d4734
Arjan Landman <landman.arjan@gmail.com>
2011-08-23 12:27:59 +0200

    routes back 2 basic

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   # Add your extension routes here
-  #match '/checkout/gateway_landing' => 'checkout#process_gateway_return', :method => :post, :as => 'gateway_landing'
-  #match '/admin/checkout/gateway_landing/:id' => 'admin/payments#process_gateway_return', :method => :post, :as => 'admin_gateway_landing'
   match '/checkout/gateway_landing' => 'checkout#process_gateway_return', :method => :post, :as => 'gateway_landing'
-  match '/checkout/gateway_landing/succes/:id' => 'checkout#process_gateway_return', :status => "success", :method => :post, :as => 'gateway_landing'
   match '/admin/checkout/gateway_landing/:id' => 'admin/payments#process_gateway_return', :method => :post, :as => 'admin_gateway_landing'
 
 end
