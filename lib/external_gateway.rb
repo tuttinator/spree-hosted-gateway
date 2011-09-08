c1929191dfff7d633a9d61958399500ffdc8eef5
Arjan Landman <landman.arjan@gmail.com>
2011-09-08 22:07:21 +0200

    made some changens for Rabobank hash

--- lib/external_gateway.rb
+++ lib/external_gateway.rb
@@ -36,7 +36,7 @@
   preference :urlsuccess, :string, :default => "http://127.0.0.1/"
   preference :urlcancel, :string, :default => "http://127.0.0.1/cancel/"
   preference :urlerror, :string, :default => "http://127.0.0.1/404/"
-  preference :secret, :string, :default => "cJqMwgU9XFatXvbR"
+  preference :secret, :string, :default => "sOgLbweLlvk9Duv1"
 
   #An array of preferences that should not be automatically inserted into the form
   INTERNAL_PREFERENCES = [:server, :status_param_key, :successful_transaction_value, :custom_data]
