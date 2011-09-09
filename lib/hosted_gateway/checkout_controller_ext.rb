aec460a6e29dc7d5c86af421ce64a10ba9b1f388
Arjan Landman <landman.arjan@gmail.com>
2011-09-09 17:03:53 +0200

    changes to work on Rabobank iDEAL Lite

--- lib/hosted_gateway/checkout_controller_ext.rb
+++ lib/hosted_gateway/checkout_controller_ext.rb
@@ -12,10 +12,7 @@ module HostedGateway
         #of this is prone to messing up updates - maybe we could use alias_method_chain or something?
 
         def process_gateway_return
-        	puts params
-        	puts ""
-        	puts ""
-        	puts params["status"] 
+        
         
           gateway = PaymentMethod.find_by_id_and_type(ExternalGateway.parse_custom_data(params)["payment_method_id"], "ExternalGateway")
           @order, payment_made = gateway.process_response(params)
@@ -36,7 +33,7 @@ module HostedGateway
 
             if @order.state == "complete" or @order.completed?
               flash[:notice] = I18n.t(:order_processed_successfully)
-              flash[:commerce_tracking] = "nothing special"
+              #flash[:commerce_tracking] = "nothing special"
               redirect_to completion_route
             else
               redirect_to checkout_state_path(@order.state)
@@ -47,6 +44,9 @@ module HostedGateway
             redirect_to checkout_path
           else
             #Error processing payment
+ 
+ 
+ 
             flash[:error] = I18n.t(:payment_processing_failed)
             redirect_to checkout_state_path(@order.state) and return
           end
