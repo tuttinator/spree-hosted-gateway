7e21e98319495d97a3e581995dbf6f147536bf45
Arjan Landman <landman.arjan@gmail.com>
2011-10-09 19:24:47 +0200

    test

--- lib/external_gateway.rb
+++ lib/external_gateway.rb
@@ -34,8 +34,8 @@
   preference :merchantid, :string, :default => "002031546"
   preference :description, :string, :default => "Evans&Watson"
   preference :ideal_urlsuccess, :string, :default => "http://evansnwatson.heroku.com"
-  preference :ideal_cancel, :string, :default => "http://evansnwatson.heroku.com/cancel"
-  preference :ideal_url_error, :string, :default => "http://evansnwatson.heroku.com/404"
+  preference :ideal_urlcancel, :string, :default => "http://evansnwatson.heroku.com/cancel"
+  preference :ideal_urlerror, :string, :default => "http://evansnwatson.heroku.com/404"
   preference :secret, :string, :default => "cJqMwgU9XFatXvbR"
 
   #An array of preferences that should not be automatically inserted into the form
