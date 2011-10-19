89656f278b01784e35509ee354b827b482791f9f
Arjan Landman <landman.arjan@gmail.com>
2011-10-19 17:19:28 +0200

    s

--- lib/external_gateway.rb
+++ lib/external_gateway.rb
@@ -198,7 +198,7 @@
 	products = Array.new();
 	
 		order.products.each_with_index do |product, i|
-			product_price = order.current_item.price.to_f * 100			
+			product_price = order.line_item.price.to_f * 100			
 			products[i] = {
 				:id => product.id,
 				:desc => product.name, 
