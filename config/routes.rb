0913fc2fb83cf3711f115c68d103656fad508e24
Arjan Landman <landman.arjan@gmail.com>
2011-10-25 16:09:00 +0200

    route update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Add your extension routes here
   match '/checkout/gateway_landing' => 'checkout#process_gateway_return', :via => :post, :as => :gateway_landing
-  match '/checkout/gateway_landing/success/:id' => 'checkout#process_gateway_return', :as => :gateway_landing, :via => :post, :defaults => {:status => 'success'}
+  #match '/checkout/gateway_landing/success/:id' => 'checkout#process_gateway_return', :as => :gateway_landing, :via => :post, :defaults => {:status => 'success'}
   #match '/checkout/:id' => 'checkout#process_gateway_return', :status => "success", :method => :post, :as => 'gateway_landing'
   #match '/checkout/gateway_landing/fail/:id' => 'checkout#process_gateway_return', :status => "fail", :method => :post, :as => 'gateway_landing'
   match '/admin/checkout/gateway_landing/:id' => 'admin/payments#process_gateway_return', :method => :post, :as => 'admin_gateway_landing'
