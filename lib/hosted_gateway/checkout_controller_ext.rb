c31f28f48fd1d1bccbf97fe9baf05d21ac544881
Arjan Landman <landman.arjan@gmail.com>
2011-10-25 19:34:24 +0200

    sd

--- lib/hosted_gateway/checkout_controller_ext.rb
+++ lib/hosted_gateway/checkout_controller_ext.rb
@@ -11,10 +11,10 @@ module HostedGateway
         #of this is prone to messing up updates - maybe we could use alias_method_chain or something?
         def process_gateway_return
               
-          #gateway = PaymentMethod.find_by_id_and_type(ExternalGateway.parse_custom_data(params)["payment_method_id"], "ExternalGateway")
-          gateway = true
-          #@order, payment_made = gateway.process_response(params)
-		  @order, payment_made = gateway
+          gateway = PaymentMethod.find_by_id_and_type(ExternalGateway.parse_custom_data(params)["payment_method_id"], "ExternalGateway")
+          
+          @order, payment_made = gateway.process_response(params)
+		  #@order, payment_made = gateway
 
           if @order && payment_made
             #Payment successfully processed
